94c526206c0fc1e3b910f23684a468a3436051e4
mrRonin <ronin.git@gmail.com>
2021-12-04 20:07:04 +0100

    added readme

---
+++ src/progetto1/core.clj
@@ -0,0 +1,11 @@
+(ns progetto1.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn diunafavata
+  (println "pierino casca in terra si sbuccia un ginocchio e se lo mangia")
+)
